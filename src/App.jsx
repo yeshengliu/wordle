4a099cba9eaefcaa176557c720b93ce908911f98
YuxuanLin-ca <YuxuanLin-ca@users.noreply.github.com>
2023-03-13 03:46:01 -0700

    added style and set up board and letter

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import { useState } from "react";
 import "./App.css";
 import Message from "./components/Message";
 import WordInput from "./components/WordInput";
-import Board from "./components/Board";
+import BoardNormal from "./components/BoardNormal";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
 
 function App() {
+  const [Board, setBoard] = useState([
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+  ]);
   return (
     <div className="App">
       <Message />
-      <Board />
+      <BoardNormal />
       <WordInput />
     </div>
   );
