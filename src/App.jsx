eb4507464a4cc403db434a271967efcddce2c9c2
YuxuanLin-ca <YuxuanLin-ca@users.noreply.github.com>
2023-03-13 05:21:37 -0700

    added set message based on situations

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardNormalDefault);
-  const [inputValue, setInputValue] = useState("");
   const [currAttempt, setCurrAttempt] = useState(0);
   const [message, setMessage] = useState(
     `Please enter a 6-letter word. ${6 - currAttempt} attempts remaining.`
@@ -21,8 +20,6 @@ function App() {
         value={{
           board,
           setBoard,
-          inputValue,
-          setInputValue,
           currAttempt,
           setCurrAttempt,
           message,
--- src/components/WordInput.jsx
+++ src/components/WordInput.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { AppContext } from "../App";
+import { useContext } from "react";
+import { useState } from "react";
 
 function WordInput() {
-  const {
-    board,
-    setBoard,
-    inputValue,
-    setInputValue,
-    currAttempt,
-    setCurrAttempt,
-  } = React.useContext(AppContext);
+  const { board, setBoard, currAttempt, setCurrAttempt, setMessage } =
+    useContext(AppContext);
+
+  const [inputValue, setInputValue] = useState("");
 
   function handleChange(e) {
     setInputValue(e.target.value.toUpperCase());
   }
 
   function handleClick() {
-    updateBoard(inputValue);
+    // if input is longer or shorter than 6 letters, do nothing
+    if (inputValue.length !== 6) {
+      setMessage("Invalid input. Please enter a 6-letter word.");
+      return;
+    }
 
-    function updateBoard() {
-      const newBoard = [...board];
-      for (let i = 0; i < 6; i++) {
-        newBoard[currAttempt][i] = inputValue[i];
-      }
-      setBoard(newBoard);
-      setCurrAttempt(currAttempt + 1);
-      setInputValue("");
+    // if input is 6-letter, add it to the board, row based on current attempt, then update currAttempt
+    const newBoard = [...board];
+    for (let i = 0; i < 6; i++) {
+      newBoard[currAttempt][i] = inputValue[i];
+    }
+    setBoard(newBoard);
+    setCurrAttempt(currAttempt + 1);
+    setInputValue("");
+
+    // if currAttemp is less than 6, show remaining attempt. if currAttemp is 6, game over
+    if (currAttempt < 5) {
+      setMessage(`${5 - currAttempt} attempts remaining.`);
+    } else {
+      setMessage("You lose!");
+      return;
     }
   }
   return (
